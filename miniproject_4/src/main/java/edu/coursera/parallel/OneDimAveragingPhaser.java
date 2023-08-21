f45038d342ac6d32baef9722be0a33244f2f17e1
dhauss <davidkhauss@gmail.com>
2023-08-21 14:51:11 -0400

    fuzzy barrier implemented

--- miniproject_4/src/main/java/edu/coursera/parallel/OneDimAveragingPhaser.java
+++ miniproject_4/src/main/java/edu/coursera/parallel/OneDimAveragingPhaser.java
@@ -110,6 +110,56 @@ public final class OneDimAveragingPhaser {
     public static void runParallelFuzzyBarrier(final int iterations,
             final double[] myNew, final double[] myVal, final int n,
             final int tasks) {
+    	 Phaser[] phs = new Phaser[tasks];
+         for(int i = 0; i < phs.length; i++){
+             phs[i] = new Phaser(1);
+         }
+
+        Thread[] threads = new Thread[tasks];
+
+        for (int ii = 0; ii < tasks; ii++) {
+            final int i = ii;
+
+            threads[ii] = new Thread(() -> {
+                double[] threadPrivateMyVal = myVal;
+                double[] threadPrivateMyNew = myNew;
+
+                final int chunkSize = (n + tasks - 1) / tasks;
+                final int left = (i * chunkSize) + 1;
+                int right = (left + chunkSize) - 1;
+                if (right > n) right = n;
+
+                for (int iter = 0; iter < iterations; iter++) {
+                    for (int j = left; j <= right; j++) {
+                        threadPrivateMyNew[j] = (threadPrivateMyVal[j - 1]
+                            + threadPrivateMyVal[j + 1]) / 2.0;
+                    }
+                    
+                    int curPhase = phs[i].arrive();
+                    
+                    if(i-1 >= 0){
+                        phs[i-1].awaitAdvance(curPhase);
+                    }
+                    
+                    if(i+1 < tasks){
+                  	  	phs[i+1].awaitAdvance(curPhase);
+                    } 
+
+                    double[] temp = threadPrivateMyNew;
+                    threadPrivateMyNew = threadPrivateMyVal;
+                    threadPrivateMyVal = temp;
+                }
+            });
+            threads[ii].start();
+        }
+
+        for (int ii = 0; ii < tasks; ii++) {
+            try {
+                threads[ii].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 }
